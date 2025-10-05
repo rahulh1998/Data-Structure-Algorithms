002cf68216101170c3fd015fe69d01603d36e168
rahulh1998 <rahul.h1998@gmail.com>
2025-10-05 18:10:58 +0530

    modified:   Searching/2. BinarySearch via      Iteration.py

--- Searching/2. BinarySearch via Iteration.py
+++ Searching/2. BinarySearch via Iteration.py
@@ -1,3 +1,25 @@
-def binarySearch(arr, target, start, end):
-    if start > end:
-        return -1
+def binarysearch(arr, target, start, end):
+
+    while start <= end:
+
+        mid = (start + end )//2
+
+        if arr[mid] == target:
+            return mid
+        
+        elif target < arr[mid]:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+    
+    return -1
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+target = 5
+res = binarysearch(arr, target, 0, len(arr)-1)
+
+if res != -1:
+    print(f"{target} present at index : {res}")
+else:
+    print(f"{target} not in {arr}")
