276048bf36fd26597c32b8af5961b2def62b375f
rahulh1998 <rahul.h1998@gmail.com>
2025-10-05 19:32:52 +0530

    modified:   Problems/O nLogn/1. 2 Sum Problem.py

--- Problems/O nLogn/1. 2 Sum Problem.py
+++ Problems/O nLogn/1. 2 Sum Problem.py
@@ -1,14 +1,12 @@
 def pair_sum_sorted_array(numbers, target):
     res = []
-    for i in range(len(numbers)):
+    for i in range(len(numbers)-1):
         for j in range(i+1, len(numbers)):
-            if numbers[i]+numbers[j] == target:
-                res.append([numbers[i],numbers[j]])
-                
+            if numbers[i] + numbers[j] == target:
+                res = [i,j]
+                return res
+
     if len(res) == 0:
         return [-1,-1]
-    else:
-        return res
-
 
 print(pair_sum_sorted_array([1, 2, 3, 5, 10], 7))
