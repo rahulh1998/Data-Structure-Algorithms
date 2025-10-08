a54cc81ea283d1b287ae8251d980c844c9dea0af
rahulh1998 <rahul.h1998@gmail.com>
2025-10-08 21:09:01 +0530

    Implemented better Code

--- Sorting/6. Quicksort Lomuto Partitioning.py
+++ Sorting/6. Quicksort Lomuto Partitioning.py
@@ -1,33 +1,32 @@
+"""
+Asymptotic complexity in terms of size of `arr` `n`:
+* Time: O(n * log(n)).
+* Auxiliary space: O(n).
+* Total space: O(n).
+"""
 import random
 
-# Quick Sort
-# Lomuto's Partitioning InPlace - O(n)
-def helperFunction(arr, start, end):
+def qsort(arr, start, end):
     if start >= end:
         return
-
-    smaller = start
-    pivotindex = random.randint(start, end - 1)
-    arr[pivotindex], arr[start] = arr[start], arr[pivotindex]
+    # Pick a random element as pivot.
+    randindex = random.randint(start, end)
+    arr[randindex], arr[start] = arr[start], arr[randindex]
     pivot = arr[start]
-
-    for bigger in range(start + 1, end):
-        if arr[bigger] < pivot:
+    smaller = start
+    bigger = start
+    for bigger in range(start + 1, end + 1):
+        if arr[bigger] <= pivot:
             smaller += 1
             arr[smaller], arr[bigger] = arr[bigger], arr[smaller]
+    # Place pivot in the right spot
+    arr[start], arr[smaller] = arr[smaller], arr[start]
+    qsort(arr, start, smaller - 1)
+    qsort(arr, smaller + 1, end)
 
-    arr[smaller], arr[start] = arr[start], arr[smaller]
-    return smaller  # partition index
-
-def quicksort(arr, start, end):
-    if start >= end:
-        return
-    
-    pivotindex = helperFunction(arr, start, end)
-    quicksort(arr, start, pivotindex)
-    quicksort(arr, pivotindex + 1, end)
+def quick_sort(arr):
+    qsort(arr, 0, len(arr) - 1)
     return arr
 
 
-arr = [5, 3, 8, 4, 2, 7, 1]
-print(quicksort(arr, 0, len(arr)))
+print(quick_sort([5, 3, 8, 4, 2, 7, 1]))
