a10296545a2b63176020daa77f2f7f6899fd4417
rahulh1998 <rahul.h1998@gmail.com>
2025-10-11 21:20:07 +0530

    modified:   Striver A2Z DSA Sheet/Problems/     Sorting/6. Find Missing No.py

--- Striver A2Z DSA Sheet/Problems/Sorting/6. Find Missing No.py
+++ Striver A2Z DSA Sheet/Problems/Sorting/6. Find Missing No.py
@@ -1,3 +1,13 @@
 #  Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. Find the number(between 1 to N), that is not present in the given array.
 
 
+def missingNumber(nums) -> int:
+        nums = sorted(nums)
+        # missing_number = -1
+        for i in range(len(nums)-1):
+            if nums[i+1] - nums[i] == 2:
+                return nums[i] + 1
+        return len(nums)
+        
+print(missingNumber([9,6,4,2,3,5,7,0,1]))
+print(missingNumber([0,1]))
