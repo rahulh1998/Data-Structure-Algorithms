680e01ab07f55f176dad165bf2632579e9978c96
rahulh1998 <rahul.h1998@gmail.com>
2025-11-05 22:08:41 +0530

    solved

--- Recursion/Easy Problems/Decimal to binary.py
+++ Recursion/Easy Problems/Decimal to binary.py
@@ -14,3 +14,11 @@
 
 # Decimal to binary conversion
 # using recursion
+
+def decimal_to_binary(n):
+    if n==0:
+        return 0
+
+    return (n%2 + 10*decimal_to_binary(n//2))
+
+print(decimal_to_binary(7))
