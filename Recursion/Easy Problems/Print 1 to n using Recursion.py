059e64d4819b8fa4818f9b4daeb7f4535ddd50d8
rahulh1998 <rahul.h1998@gmail.com>
2025-11-05 08:48:02 +0530

    modified the code

--- Recursion/Easy Problems/Print 1 to n using Recursion.py
+++ Recursion/Easy Problems/Print 1 to n using Recursion.py
@@ -1,7 +1,14 @@
 def print_1_to_n(n):
-    while n>0:
-        print_1_to_n(n-1)
-        print(n)
+    # # Approach 1
+    # while n>0:
+    #     print_1_to_n(n-1)
+    #     print(n)
+    #     return
+    
+    # Approach 2
+    if n==0:
         return
-
+    print_1_to_n(n-1)
+    print(n)
+        
 print_1_to_n(10)
