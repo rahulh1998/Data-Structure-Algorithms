42c9c885fc769fed00a2c9569296af58a187f11e
rahulh1998 <rahul.h1998@gmail.com>
2025-11-05 09:58:31 +0530

    solved reverse string

---
+++ Recursion/Easy Problems/Decimal to binary.py
@@ -0,0 +1,16 @@
+# Given a decimal number as input, we need to write a program to convert the given decimal number into an equivalent binary number. 
+
+# Examples : 
+
+# Input: d = 7                                                         
+# Output: 111
+# Explanation:  2^0 + 2^1  + 2^2 = 1+2+4 = 7.
+
+# Input: d = 10
+# Output: 1010
+# Explanation: 2^1  + 2^3 = 2+8 = 10.
+
+
+
+# Decimal to binary conversion
+# using recursion
---
+++ Recursion/Easy Problems/Reverse a String.py
@@ -0,0 +1,6 @@
+def reverse_string(string):
+    if len(string) == 0:
+        return ""
+    return str(string[-1]) + reverse_string(string[:-1])
+
+print(reverse_string('memo'))
