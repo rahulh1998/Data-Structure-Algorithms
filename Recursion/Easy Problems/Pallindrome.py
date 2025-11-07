54eda4dc8fd12b063b573eb59acf1fd4d04b05a2
rahulh1998 <rahul.h1998@gmail.com>
2025-11-07 07:00:07 +0530

    new file:   Recursion/Easy Problems/Pallindrome.py

---
+++ Recursion/Easy Problems/Pallindrome.py
@@ -0,0 +1,20 @@
+def pallindrome(s):
+
+    res =  helper(s, 0, len(s) - 1)
+    if res:
+        print("true")
+    else:
+        print("false")
+
+def helper(s, left, right):
+    if left>=right:
+        return True
+    
+    if  s[left] != s[right]:
+        return False
+
+    return helper(s, left+1,right-1)
+
+
+pallindrome('aba')
+
